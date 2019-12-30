8be4411a29a30bde809bfc4c98c1a8738c32fd1f
Jorge Santos <neneluis55@gmail.com>
2019-12-30 08:59:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,7 +27,7 @@ end
 def list_of_directors(source)
   # Write this implementation
   require 'pp'
-result = { 
+stooges = { 
   "Stephen Spielberg"=>1357566430,
         "Russo Brothers"=>2281002470,
         "James Cameron"=>2571020373,
@@ -37,7 +37,7 @@ result = {
         "Quentin Tarantino"=>662738268,
         "Martin Scorsese"=>636812242,
         "Francis Ford Coppola"=>509719258}
-        p result
+        p stooges
 end
 
 def total_gross(collection)
